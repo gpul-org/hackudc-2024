00a597f1173682dea6b0dc2e9175975f82f460a3
Jorge <jorge.teixeira@bizaway.com>
2023-11-22 14:19:26 +0100

    add links to sponsors and center

--- src/types.d.ts
+++ src/types.d.ts
@@ -233,9 +233,9 @@ export interface Testimonials extends Headline, Widget {
 }
 
 export interface Brands extends Headline, Widget {
-  oro?: Array<Image & { height: number, width: number }>;
-  plata?: Array<Image & { height: number, width: number }>;
-  colaboradores?: Array<Image & { height: number, width: number }>;
+  oro?: Array<Image & { height: number, width: number, href: string }>;
+  plata?: Array<Image & { height: number, width: number, href: string }>;
+  colaboradores?: Array<Image & { height: number, width: number, href: string }>;
 }
 
 export interface Features extends Headline, Widget {
